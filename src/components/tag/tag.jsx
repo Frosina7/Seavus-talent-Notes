d80de319eaf1bf52d8f14785b9c8e014d3310a19
Frose <gicevafrose@gmail.com>
2020-10-06 19:44:54 +0200

    Created tags manager

--- src/components/tag/tag.jsx
+++ src/components/tag/tag.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 import "./tag_style.css";
+import axios from "axios";
 import { Box } from "@material-ui/core";
 
 class TagsManager extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`/api/tags`).then((res) => {
+      console.log(res);
+      this.setState({ tags: res.data });
+    });
+  }
+
+  deleteTag = () => {
+    axios.delete(`api/tags/${this.props.id}`);
+    window.location.reload(true);
+  };
+
   render() {
     return (
       <div className='tag-create-form'>
@@ -21,6 +42,30 @@ class TagsManager extends Component {
                 Create
               </button>
             </Box>
+            <div className='container' id='tags-container'>
+              {this.state.tags.map((tag, index) => (
+                <div className='tag-button-container'>
+                  <div
+                    type='text'
+                    className='form-control'
+                    id='form-tags'
+                    key={index}
+                    id={tag.id}
+                  >
+                    #{tag.name}
+                  </div>
+                  <div>
+                    <button
+                      type='button'
+                      className='btn btn'
+                      onClick={this.deleteTag}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </form>
       </div>
